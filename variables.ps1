6cbfbffefd5c57daa3ba34448eb95adc6287a0fd
Alpha004 <jesusalren93@gmail.copm>
2026-07-21 20:11:29 -0500

    feat: added correct subscriptionId and another NODE_SIZE due to an error 'The VM size of Standard_D2s_v3 is not allowed in your subscription in location 'eastus' #2

--- variables.ps1
+++ variables.ps1
@@ -3,13 +3,13 @@ $LOC = "eastus"
 
 # AKS
 $AKS_NAME = "aks-cardops-demo"
-$AKS_NODE_SIZE = "Standard_D2s_v3"
+$AKS_NODE_SIZE = "Standard_D2as_v7"
 $AKS_NODE_COUNT = 1
 
 # APIM
 $APIM_NAME = "apim-cardops-demo"
 
-$SUBSCRIPTION_ID = "4a42d9ba-d900-40bd-93ea-6e3548f68daa"
+$SUBSCRIPTION_ID = "d61407c3-acb8-4259-a7af-fa733c23950a"
 
 # COSMOS (Mongo API)
 $COSMOS_ACCOUNT = "cosmos-cardops-demo"   # unico en Azure
